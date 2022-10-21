c5145146549f4e7d5672f8b701069aff9a49256a
tarun dahiya <tarundh1997@gmail.com>
2022-10-21 21:07:40 +0530

    working on api and response

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -8,41 +9,67 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"))
 
-app.post("/", function (req, res){
-    var firstName = req.body.fName
-    var lastName = req.body.lName
-    var email = req.body.email
-    console.log(firstName, lastName, email);
-});
-
-app.get("/", (req, res)=> {
+app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html")
 })
+
+app.post("/", function (req, res){
+    const firstName = req.body.fName
+    const lastName = req.body.lName
+    const email = req.body.email
+    
+    const data = {
+        members : [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us21.api.mailchimp.com/3.0/lists/fca3130ef2"
+
+    const options = {
+        method : "POST",
+        auth : "tarun:7b6d8193bcf9e84a237c4e5a46932e89-us21"
+    }
+
+    const request = https.request(url, options, function(response){
+
+        if (response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html")
+        } else {
+            res.sendFile(__dirname + "/failure.html")
+        }
+
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData); 
+    request.end();
+});
+
+
+app.post("/failure", function(req, res){
+    res.redirect("/")
+});
+
 
 app.listen(3000, (req, res)=>{
     console.log("server is running on port 3000")
 })
 
 //API key
-// dab07c8ea483e7bd90eee4d530ef2e98-us21
+// 7b6d8193bcf9e84a237c4e5a46932e89-us21
 
 // list id
 // fca3130ef2
 
---data @- \
-<<EOF | jq '.id'
-{
-  "email_address": "$user_email",
-  "status": "pending",
-  "merge_fields": {
-	"FNAME": "$user_fname",
-	"LNAME": "$user_lname",
-	"BIRTHDAY": "$user_birthday",
-	"ADDRESS": {
-           "addr1": "123 Freddie Ave",
-           "city": "Atlanta",
-           "state": "GA",
-           "zip": "12345",
-
-     }
-  }
